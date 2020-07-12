093a7e5c16f637ea01262dd903457e42c76bb910
Miguel Pimentel <miguel.silva@tembici.com>
2020-07-12 19:55:35 -0300

    Added Hello World

--- DangerSample/Controller/ViewController.swift
+++ DangerSample/Controller/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+
+    @IBOutlet weak var sampleLabel: UILabel!
+
+    // MARK: - View Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         helloWorld()
     }
 
+    // MARK: - Private Methods
+
     private func helloWorld() {
-        if true {
-            print("Hello World")
-            view.backgroundColor = .gray
+        if true{
+            sampleLabel.text = "Hello World!"
         }
     }
 }
+
+
